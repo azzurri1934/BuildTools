2176add4af8b6a8e1ce97fccbb7167f1ad8c98db
azzurri1934 <azzurri1934@gmail.com>
2018-12-19 18:33:57 +0900

    Update BuildTools.sh

--- BuildTools.sh
+++ BuildTools.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 git config --global --unset core.autocrlf
-sh 'java -Xmx1024M -jar BuildTools.jar --rev $1
+java -Xmx1024M -jar BuildTools.jar --rev $1
